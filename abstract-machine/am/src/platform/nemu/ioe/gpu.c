55bcf53f7e176e8120d7987b622478ed30b44aa1
zhangshenlu <2444967582@qq.com>
2026-03-06 23:08:03 +0800

    PA2Done

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -7,14 +7,35 @@ void __am_gpu_init() {
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
+  uint32_t vgactl = inl(VGACTL_ADDR);
+  uint32_t width = vgactl >> 16;  // 高16位是宽度
+  uint32_t height = vgactl & 0xFFFF;  // 低16位是高度
+  
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = 0, .height = 0,
-    .vmemsz = 0
+    .width = width, .height = height,
+    .vmemsz = width * height * sizeof(uint32_t)
   };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  uint32_t *pixels = (uint32_t *)ctl->pixels;
+  
+  // 获取屏幕宽度
+  AM_GPU_CONFIG_T cfg;
+  __am_gpu_config(&cfg);
+  int screen_w = cfg.width;
+  
+  // 将像素数据写入帧缓冲
+  for (int j = 0; j < ctl->h; j++) {
+    for (int i = 0; i < ctl->w; i++) {
+      int screen_idx = (ctl->y + j) * screen_w + (ctl->x + i);
+      int pixel_idx = j * ctl->w + i;
+      fb[screen_idx] = pixels[pixel_idx];
+    }
+  }
+  
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
